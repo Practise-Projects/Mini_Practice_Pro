804e00b5653f559be866aef7c49095faff704035
SNEH <snehgour80@gmail.com>
2020-03-10 04:15:05 +0530

    models.py add

--- mampro/mamapp/models.py
+++ mampro/mamapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class College(models.Model):
+    College_Name=models.CharField(max_length=200)
+    College_id=models.IntegerField()
+    Department=models.CharField(max_length=100)
+
+class student(models.Model): 
+    college=models.ForeignKey(College,on_delete=models.CASCADE)
+    Student_Name=models.CharField(max_length=60)
+    Enrollment_no=models.IntegerField(primary_key=True)
+    Dob=models.DateTimeField()
+    Cgpa=models.IntegerField()
+    Address=models.CharField(max_length=100)
+    College_Name=models.CharField(max_length=200)
+    Branch=models.CharField(max_length=50)
+
