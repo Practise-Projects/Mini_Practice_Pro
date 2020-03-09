95524e3e815d013bdce44438996fb44f674b1ec7
unknown <dubeybulbul111@gmail.com>
2020-03-09 02:57:18 -0700

    only project making for checking access

---
+++ mampro/mampro/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mampro project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mampro.settings')
+
+application = get_asgi_application()
