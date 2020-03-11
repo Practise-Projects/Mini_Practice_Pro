ca8bbaa41bc7bb380aad96a1275ff111afd9fcdc
unknown <dubeybulbul111@gmail.com>
2020-03-11 15:04:39 -0700

    hII

--- mampro/mampro/urls.py
+++ mampro/mampro/urls.py
@@ -9,18 +9,13 @@ Function views
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+#from django.conf.urls import include
 from testapp import views
 #from mampro import testapp
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #url(r'^admin/', admin.site.urls),
-    #url(r'^testapp/', include('testapp.urls')),
-    #path('testapp/',include('testapp.urls')),
     path('', views.student, name='student'),
     path('college/', views.college, name='college'),
-    path('(?P<stu_id>[0-9]+)/',views.college ,name='college'),
+    path('hii/<int:stu_id>/',views.college ,name='college'),
 ]
-
-    #url(r'^(?P<stu_id>[0-9]+)/$',views.College ,name='College'),
 
--- mampro/testapp/views.py
+++ mampro/testapp/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 from .models import student
 from django.http import Http404
 
-def student (request , stu_id ):
-
-    try:
-        stu = student.objects.get(pk=stu_id)
-    except student.DoesNotExist:
-        raise Http404("Student does not exist")
-    return render(request, 'testapp/college.html',  {'stu':stu } )
 
 def college(request):
     all_stu = student.objects.all()
     context = {'all_stu': all_stu }
     return render(request, 'testapp/student.html' ,context )
+
+def student (request , stu_id ):
+    try:
+        stu = student.objects.get(pk=stu_id)
+    except student.DoesNotExist:
+        raise Http404("Student does not exist")
+    return render(request, 'testapp/college.html',  {'stu':stu } )
 
